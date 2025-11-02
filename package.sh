a41d120d545796bc91cc0992b8a690d993c92119
TheGreatGooo <thegreatgooo@gmail.com>
2025-11-02 17:14:11 -0500

    Update package.sh

--- package.sh
+++ package.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 mkdir -p chromePackage
 cd chromePackage
-cp ../build/src/out/Default/* . || true
-cp -R ../build/src/out/Default/angledata .
-cp -R ../build/src/out/Default/locales .
-cp -R ../build/src/out/Default/resources .
-rm -rf .ninja_deps .ninja_log build.ninja
+cp ../build/src/out/Default/chrome .
+cp ../build/src/out/Default/chromedriver .
+cp ../build/src/out/Default/chrome_sandbox .
+cp ../build/src/out/Default/chrome-wrapper .
+cp -r ../build/src/out/Default/locales .
+cp ../build/src/out/Default/*.bin .
+cp ../build/src/out/Default/*.so .
+cp ../build/src/out/Default/*.pak .
