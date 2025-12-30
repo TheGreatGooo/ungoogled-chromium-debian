1658823e8805261ba223c642250cfc7e9b0a4a36
Srujith <srujith@kudikala.com>
2025-12-29 19:27:41 -0500

    add sysroot build

--- build.3.sh
+++ build.3.sh
@@ -5,6 +5,12 @@ mkdir -p build/src/out/Default
 cp buildScript/ungoogled-chromium-debian-main/args.gn build/src/out/Default/args.gn
 cd build/src
 echo "7----------------------------------------------------------------"
+pushd $WORKSPACE/build/src/build/linux/sysroot_scripts
+python3 -m venv .venv
+source .venv/bin/activate
+./generate_keyring.sh
+python3 sysroot_creator.py build amd64
+popd
 patch -u tools/clang/scripts/build.py -i ../../buildScript/ungoogled-chromium-debian-main/patches/clang_build.patch 
 patch -u tools/rust/build_rust.py -i ../../buildScript/ungoogled-chromium-debian-main/patches/rust_build.patch
 patch -u tools/rust/build_bindgen.py -i ../../buildScript/ungoogled-chromium-debian-main/patches/build_bindgen.patch
