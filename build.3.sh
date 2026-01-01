052a72cc1f1ee243be3e2a80553a6e25fea776a3
Srujith <srujith@kudikala.com>
2026-01-01 15:27:55 -0500

    mock version

--- build.3.sh
+++ build.3.sh
@@ -11,6 +11,13 @@ source .venv/bin/activate
 pip3 install requests
 ./generate_keyring.sh
 python3 sysroot_creator.py build amd64
+LLVM_BUILD_TOOLS_DIR="$WORKSPACE/build/src/third_party/llvm-build-tools"
+mkdir -p $LLVM_BUILD_TOOLS_DIR
+DEBIAN_SYSROOT_EXPECTED="$LLVM_BUILD_TOOLS_DIR/debian_bullseye_amd64_sysroot"
+pushd $DEBIAN_SYSROOT_EXPECTED
+tar xvf "$WORKSPACE/build/src/out/sysroot-build/bullseye/debian_bullseye_amd64_sysroot.tar.xz"
+echo "dec7a3a0fc5b83b909cba1b6d119077e0429a138eadef6bf5a0f2e03b1904631" >> "$DEBIAN_SYSROOT_EXPECTED/stamp"
+popd
 popd
 patch -u tools/clang/scripts/build.py -i ../../buildScript/ungoogled-chromium-debian-main/patches/clang_build.patch 
 patch -u tools/rust/build_rust.py -i ../../buildScript/ungoogled-chromium-debian-main/patches/rust_build.patch
