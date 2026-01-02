30eb958611070c659bbce7da70fe4d370b48d9b4
Srujith <srujith@kudikala.com>
2026-01-01 19:23:03 -0500

    add sysroot

--- build.sysroot.sh
+++ build.sysroot.sh
@@ -1,15 +1,12 @@
-#!/bin/bash
-set -e -x
-echo "6----------------------------------------------------------------"
-mkdir -p build/src/out/Default
-cp buildScript/ungoogled-chromium-debian-main/args.gn build/src/out/Default/args.gn
-cd build/src
-echo "7----------------------------------------------------------------"
+mkdir -p build/src
+pushd build/src
+#wget https://chromium.googlesource.com/chromium/src/+archive/a98fe8c2f2e7c84dc95879fbc5a11f5de7662353.tar.gz
+tar xvzf a98fe8c2f2e7c84dc95879fbc5a11f5de7662353.tar.gz
+
 pushd $WORKSPACE/build/src/build/linux/sysroot_scripts
 python3 -m venv .venv
 source .venv/bin/activate
 pip3 install requests
-./generate_keyring.sh
 
 ARCH="amd64"
 python3 sysroot_creator.py build $ARCH
@@ -50,23 +47,3 @@ pushd $DEBIAN_SYSROOT_EXPECTED
 tar xvf "$WORKSPACE/build/src/out/sysroot-build/bullseye/debian_bullseye_${ARCH}_sysroot.tar.xz"
 echo "308e23faba3174bd01accfe358467b8a40fad4db4c49ef629da30219f65a275f" > "$DEBIAN_SYSROOT_EXPECTED/stamp"
 popd
-
-ARCH="riscv64"
-python3 sysroot_creator.py build $ARCH
-LLVM_BUILD_TOOLS_DIR="$WORKSPACE/build/src/third_party/llvm-build-tools"
-DEBIAN_SYSROOT_EXPECTED="$LLVM_BUILD_TOOLS_DIR/debian_trixie_${ARCH}_sysroot"
-mkdir -p $DEBIAN_SYSROOT_EXPECTED
-pushd $DEBIAN_SYSROOT_EXPECTED
-tar xvf "$WORKSPACE/build/src/out/sysroot-build/trixie/debian_trixie_${ARCH}_sysroot.tar.xz"
-echo "6c924a8f88bb4731f3c2334c6ae5b5da47d5ca196ff571a91071f104dbacecad" > "$DEBIAN_SYSROOT_EXPECTED/stamp"
-popd
-
-
-popd
-patch -u tools/clang/scripts/build.py -i ../../buildScript/ungoogled-chromium-debian-main/patches/clang_build.patch 
-patch -u tools/rust/build_rust.py -i ../../buildScript/ungoogled-chromium-debian-main/patches/rust_build.patch
-patch -u tools/rust/build_bindgen.py -i ../../buildScript/ungoogled-chromium-debian-main/patches/build_bindgen.patch
-tools/clang/scripts/build.py --host-cc /usr/bin/clang --host-cxx /usr/bin/clang++ --use-system-cmake --with-ml-inliner-model='' --without-android --without-fuchsia  
-tools/rust/build_rust.py --skip-test
-tools/rust/build_bindgen.py
-cp third_party/rust-toolchain/VERSION third_party/rust-toolchain/INSTALLED_VERSION
