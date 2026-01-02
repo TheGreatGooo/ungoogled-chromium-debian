ceb2d4ec51af17d4bdd0e4172fdb313ab2b034b4
Srujith <srujith@kudikala.com>
2026-01-01 19:23:58 -0500

    add riscv64

--- build.sysroot.sh
+++ build.sysroot.sh
@@ -47,3 +47,23 @@ pushd $DEBIAN_SYSROOT_EXPECTED
 tar xvf "$WORKSPACE/build/src/out/sysroot-build/bullseye/debian_bullseye_${ARCH}_sysroot.tar.xz"
 echo "308e23faba3174bd01accfe358467b8a40fad4db4c49ef629da30219f65a275f" > "$DEBIAN_SYSROOT_EXPECTED/stamp"
 popd
+
+mkdir -p $WORKSPACE/build/src.riscv64
+pushd $WORKSPACE/build/src.riscv64
+wget https://chromium.googlesource.com/chromium/src/+archive/7cc2fd642bdcee70fc21d8a0f9e01cfa26624e94.tar.gz
+tar xvzf 7cc2fd642bdcee70fc21d8a0f9e01cfa26624e94.tar.gz
+
+pushd $WORKSPACE/build/src.riscv64/build/linux/sysroot_scripts
+python3 -m venv .venv
+source .venv/bin/activate
+pip3 install requests
+
+ARCH="riscv64"
+python3 sysroot_creator.py build $ARCH
+LLVM_BUILD_TOOLS_DIR="$WORKSPACE/build/src.riscv64/third_party/llvm-build-tools"
+DEBIAN_SYSROOT_EXPECTED="$LLVM_BUILD_TOOLS_DIR/debian_trixie_${ARCH}_sysroot"
+mkdir -p $DEBIAN_SYSROOT_EXPECTED
+pushd $DEBIAN_SYSROOT_EXPECTED
+tar xvf "$WORKSPACE/build/src.riscv64/out/sysroot-build/trixie/debian_trixie_${ARCH}_sysroot.tar.xz"
+echo "6c924a8f88bb4731f3c2334c6ae5b5da47d5ca196ff571a91071f104dbacecad" > "$DEBIAN_SYSROOT_EXPECTED/stamp"
+popd
