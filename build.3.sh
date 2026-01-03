970aa97e5a941be42d06e8ebe4668a99e574f2c1
Srujith <srujith@kudikala.com>
2026-01-03 18:46:49 -0500

    seperate out llvm build

--- build.3.sh
+++ build.3.sh
@@ -58,11 +58,8 @@ popd
 
 
 popd
-patch -u tools/clang/scripts/build.py -i ../../buildScript/ungoogled-chromium-debian-main/patches/clang_build.patch 
 patch -u tools/rust/build_rust.py -i ../../buildScript/ungoogled-chromium-debian-main/patches/rust_build.patch
 patch -u tools/rust/build_bindgen.py -i ../../buildScript/ungoogled-chromium-debian-main/patches/build_bindgen.patch
-sed -i "s|CLANG_REVISION = 'llvmorg-22-init-8940-g4d4cb757'|CLANG_REVISION = '292dc2b86f66e39f4b85ec8b185fd8b60f5213ce'|g" "$WORKSPACE/build/src/tools/rust/update_rust.py"
-tools/clang/scripts/build.py --host-cc /usr/bin/clang --host-cxx /usr/bin/clang++ --use-system-cmake --with-ml-inliner-model='' --without-android --without-fuchsia  
 sed -i "s|RUST_REVISION = 'f8297e351a40c1439a467bbbb6879088047f50b3'|RUST_REVISION = 'ded5c06cf21d2b93bffd5d884aa6e96934ee4234'|g" "$WORKSPACE/build/src/tools/rust/update_rust.py"
 tools/rust/build_rust.py --skip-test
 tools/rust/build_bindgen.py
