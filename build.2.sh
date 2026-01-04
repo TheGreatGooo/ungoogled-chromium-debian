3e206e79577f8e00606365cff4eb57ca37a5adf2
Srujith <srujith@kudikala.com>
2026-01-04 13:26:09 -0500

    patch beforehand

--- build.2.sh
+++ build.2.sh
@@ -64,3 +64,13 @@ popd
 
 
 popd
+
+cd $WORKSPACE
+pushd build/src
+patch -u tools/rust/build_rust.py -i ../../buildScript/ungoogled-chromium-debian-main/patches/rust_build.patch
+patch -u tools/rust/build_bindgen.py -i ../../buildScript/ungoogled-chromium-debian-main/patches/build_bindgen.patch
+patch -u tools/clang/scripts/build.py -i ../../buildScript/ungoogled-chromium-debian-main/patches/clang_build.patch 
+
+sed -i "s|CLANG_REVISION = 'llvmorg-22-init-8940-g4d4cb757'|CLANG_REVISION = '6d31e7e4f29e89213a55e9f5fdcd176396604637'|g" "$WORKSPACE/build/src/tools/clang/scripts/update.py"
+sed -i "s|GitCherryPick(LLVM_DIR|#GitCherryPick(LLVM_DIR|g" "$WORKSPACE/build/src/tools/clang/scripts/build.py"
+sed -i "s|RUST_REVISION = 'f8297e351a40c1439a467bbbb6879088047f50b3'|RUST_REVISION = 'ded5c06cf21d2b93bffd5d884aa6e96934ee4234'|g" "$WORKSPACE/build/src/tools/rust/update_rust.py"
