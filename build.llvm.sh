07edba0be6b4242992b16d3ba727837d5960ef5b
Srujith <srujith@kudikala.com>
2026-01-03 20:39:36 -0500

    typo

--- build.llvm.sh
+++ build.llvm.sh
@@ -4,6 +4,6 @@ pushd build/src
 patch -u tools/clang/scripts/build.py -i ../../buildScript/ungoogled-chromium-debian-main/patches/clang_build.patch 
 sed -i "s|CLANG_REVISION = 'llvmorg-22-init-8940-g4d4cb757'|CLANG_REVISION = '292dc2b86f66e39f4b85ec8b185fd8b60f5213ce'|g" "$WORKSPACE/build/src/tools/clang/scripts/update.py"
 sed -i "s|GitCherryPick(LLVM_DIR|#GitCherryPick(LLVM_DIR|g" "$WORKSPACE/build/src/tools/clang/scripts/update.py"
-ools/clang/scripts/build.py --host-cc /usr/bin/clang --host-cxx /usr/bin/clang++ --use-system-cmake --with-ml-inliner-model='' --without-android --without-fuchsia  
+tools/clang/scripts/build.py --host-cc /usr/bin/clang --host-cxx /usr/bin/clang++ --use-system-cmake --with-ml-inliner-model='' --without-android --without-fuchsia  
 # build/src/third_party/llvm-build-tools
 # build/src/third_party/llvm-build
