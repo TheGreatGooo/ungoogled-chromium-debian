28cf1f25ee3748d216272d38ffc0172de10b8ede
Srujith <srujith@kudikala.com>
2026-01-04 15:35:09 -0500

    use built system roots

--- build.rust.sh
+++ build.rust.sh
@@ -4,3 +4,4 @@ pushd build/src
 tools/rust/build_rust.py --skip-test
 tools/rust/build_bindgen.py
 cp third_party/rust-toolchain/VERSION third_party/rust-toolchain/INSTALLED_VERSION
+# build/src/third_party/rust-toolchain
