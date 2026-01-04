9162ba5a962ed288e522bc8fa0c49d7ad5ce94f0
Srujith <srujith@kudikala.com>
2026-01-04 18:15:17 -0500

    compatibility hack

--- build.rust.sh
+++ build.rust.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -ex
 pushd build/src
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$WORKSPACE/build/src/third_party/llvm-build-tools/debian_bullseye_amd64_sysroot/usr/lib/x86_64-linux-gnu
 tools/rust/build_rust.py --skip-test
 tools/rust/build_bindgen.py
 cp third_party/rust-toolchain/VERSION third_party/rust-toolchain/INSTALLED_VERSION
