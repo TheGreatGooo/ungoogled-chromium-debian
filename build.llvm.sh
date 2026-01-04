6297efe6e29d2780415e4191a751350f9f965bef
Srujith <srujith@kudikala.com>
2026-01-03 22:23:07 -0500

    try something really new

--- build.llvm.sh
+++ build.llvm.sh
@@ -2,7 +2,7 @@
 set -e
 pushd build/src
 patch -u tools/clang/scripts/build.py -i ../../buildScript/ungoogled-chromium-debian-main/patches/clang_build.patch 
-sed -i "s|CLANG_REVISION = 'llvmorg-22-init-8940-g4d4cb757'|CLANG_REVISION = '8e2cd28cd4ba46613a46467b0c91b1cabead26cd'|g" "$WORKSPACE/build/src/tools/clang/scripts/update.py"
+sed -i "s|CLANG_REVISION = 'llvmorg-22-init-8940-g4d4cb757'|CLANG_REVISION = '6d31e7e4f29e89213a55e9f5fdcd176396604637'|g" "$WORKSPACE/build/src/tools/clang/scripts/update.py"
 sed -i "s|GitCherryPick(LLVM_DIR|#GitCherryPick(LLVM_DIR|g" "$WORKSPACE/build/src/tools/clang/scripts/build.py"
 tools/clang/scripts/build.py --host-cc /usr/bin/clang --host-cxx /usr/bin/clang++ --use-system-cmake --with-ml-inliner-model='' --without-android --without-fuchsia  
 # build/src/third_party/llvm-build-tools
