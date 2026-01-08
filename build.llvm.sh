1c028f0021d513b53a02d5d077e01797004cbc54
Srujith <srujith@kudikala.com>
2026-01-08 10:23:56 -0500

    set directory

--- build.llvm.sh
+++ build.llvm.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -ex
+pushd build/src
 tools/clang/scripts/build.py --host-cc /usr/bin/clang --host-cxx /usr/bin/clang++ --use-system-cmake --with-ml-inliner-model='' --without-android --without-fuchsia  
 # build/src/third_party/llvm-build-tools
 # build/src/third_party/llvm-build
+popd
