a69362d1fb43da46cd08bda2f3f35fde5081f2da
Srujith <srujith@kudikala.com>
2026-01-08 10:44:37 -0500

    add cpp args and qt6 support

--- build.5.sh
+++ build.5.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 set -ex
+CLANG_PATH=$WORKSPACE/build/src/third_party/llvm-build/Release+Asserts/bin
+export CC=$CLANG_PATH/clang
+export CXX=$CLANG_PATH/clang++
+export AR=$CLANG_PATH/llvm-ar
+export NM=$CLANG_PATH/llvm-nm
 cat "flags.gn" "buildScript/ungoogled-chromium-debian-main/args.gn" | tee "build/src/out/Default/args.gn"
 cd build/src
 echo "9----------------------------------------------------------------"
--- build.6.sh
+++ build.6.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 set -ex
 cd build/src
+CLANG_PATH=$WORKSPACE/build/src/third_party/llvm-build/Release+Asserts/bin
+export CC=$CLANG_PATH/clang
+export CXX=$CLANG_PATH/clang++
+export AR=$CLANG_PATH/llvm-ar
+export NM=$CLANG_PATH/llvm-nm
 echo "10----------------------------------------------------------------"
 ninja -C out/Default chrome chromedriver chrome_sandbox
